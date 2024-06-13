cae19f90914f068504a0efc24959a48a9d177316
slondonoq <slondonoq@unal.edu.co>
2024-06-13 18:00:01 -0500

    Changes to test the idea to improve output gen

--- LatinoToJs.py
+++ LatinoToJs.py
@@ -6,8 +6,8 @@ from translations.assignations import *
 class LatinoToJs(LatinoGrammarListener):
 
     def __init__(self):
-        self.symbolArray = [] # Used to keep track of defined variables
-        self.indentationStack = [] # Use to keep indentation levels when nesting blocks
+        self.symbolArray = []  # Used to keep track of defined variables
+        self.indentationStack = []  # Use to keep indentation levels when nesting blocks
         self.jsCode = ''
         self.passSemiColon = [';', '/', '}','\n']
         self.boolean_and_null_translation = {
@@ -44,6 +44,46 @@ class LatinoToJs(LatinoGrammarListener):
 
     def enterAssig(self, ctx:LatinoGrammarParser.AssigContext):
         enterAssignationRule(self, ctx)
+
+    def enterExp(self, ctx:LatinoGrammarParser.ExpContext):
+
+        string_replacement = f'?~terminal'
+
+        # Add terminal placeholders if there are binary operations
+        operator = ''
+        # TODO: translation of string operators
+        for binary_op in ctx.binaryOp():
+            if binary_op.L_UNARY_OP():
+                operator = binary_op.L_UNARY_OP().getText()
+            elif binary_op.NUMERIC_OP():
+                operator = binary_op.NUMERIC_OP().getText()
+            elif binary_op.LOGIC_OP():
+                operator = binary_op.LOGIC_OP().getText()
+            else:
+                operator = binary_op.STRING_OP().getText()
+
+            string_replacement += f' {operator} ?~terminal'
+
+        self.jsCode = self.jsCode.replace('?~exp', string_replacement, 1)
+
+    def enterTerminal(self, ctx:LatinoGrammarParser.TerminalContext):
+
+        string_to_replace = ''
+        # TODO: complete the cases, hopefully it won't become spaghetti
+        if ctx.REAL():
+            string_to_replace = ctx.REAL().getText()
+        elif ctx.STRING():
+            string_to_replace = ctx.STRING().getText()
+        elif ctx.BOOLEAN_VALS():
+            string_to_replace = self.boolean_and_null_translation[ctx.BOOLEAN_VALS().getText()]
+        elif ctx.NULL_VAL():
+            string_to_replace = self.boolean_and_null_translation[ctx.NULL_VAL().getText()]
+        elif ctx.OPENING_PAR():
+            string_to_replace = f'(?~exp)'
+        elif ctx.L_UNARY_OP():
+            string_to_replace = f'{ctx.L_UNARY_OP().getText()} ?~exp'
+
+        self.jsCode = self.jsCode.replace('?~terminal', string_to_replace, 1)
 
     def exitSource(self, ctx:LatinoGrammarParser.SourceContext):
         print("----------------------JS CODE----------------------")
